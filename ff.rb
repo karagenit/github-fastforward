e08e9d208edcfa3ff9bfc2e7fe62c345c19ecdbf
Caleb Smith <karagenit@outlook.com>
2017-08-01 04:16:36 +0000

    Add Script

--- ff.rb
+++ ff.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 
-if `git branch | grep -q master`
-	puts hi
+parent = "upstream"
+
+unless `git branch`.include? parent
+
+    print "Parent URL: "
+    url = gets.chomp
+
+    `git remote add #{parent} #{url}`
 end
+
+branch = `git rev-parse --abbrev-ref HEAD`.chomp
+
+`git fetch #{parent} #{branch}`
+
+`git rebase #{parent}/#{branch} #{branch}`
