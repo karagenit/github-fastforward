41f15acc33ad32322daea4c7078940715075fcb8
Caleb Smith <karagenit@outlook.com>
2017-08-02 02:56:00 +0000

    Add HTTP Request

--- ff.rb
+++ ff.rb
@@ -1,11 +1,28 @@
 #!/usr/bin/env ruby
 
+require "net/http"
+require "uri"
+require "json"
+
 parent = "upstream"
+repo = `git config --get remote.origin.url`.sub(/https:\/\/github.com\//, '').chomp
 
 unless `git branch`.include? parent
 
-    print "Parent URL: "
-    url = gets.chomp
+    uri = URI.parse(URI.encode("https://api.github.com/repos/#{repo}")) #TODO i think encode is only necessary w/o repo.chomp
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    data = http.request(request).body
+
+    puts JSON.pretty_generate(JSON.parse(data))
+
+    parent = JSON.parse(data)['parent']
+
+    puts parent
+
+    exit
 
     `git remote add #{parent} #{url}`
 end
